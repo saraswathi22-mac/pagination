942320cddc376a296ba2e79c92bc0f45eea10d12
exit <saraswathi.mac@gmail.com>
2023-12-19 15:20:56 +0530

    Updated Header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header>
+      <nav className="flex justify-between items-center h-12 px-4 shadow-md bg-gray-800 text-white">
+        <Link href="/" className="text-lg font-bold">
+          Zenith World
+        </Link>
+        <div>
+          <span className="cart-badge">0</span>
+          <Link href="/cart">Cart</Link>
+        </div>
+      </nav>
+    </header>
+  );
+}

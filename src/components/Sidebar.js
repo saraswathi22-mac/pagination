72e919fe21a06621a9f06b3ce312cbf909588f11
exit <saraswathi.mac@gmail.com>
2023-12-19 15:34:59 +0530

    Added sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,6 @@
 export default function CartSidebar() {
   return (
-    <div className="fixed top-0 right-0 w-32 h-full shadow-lg border-l border-l-gary-700 overflow-scroll">
+    <div className="fixed top-0 right-0 w-44 h-full shadow-lg border-l border-l-gary-700 overflow-scroll">
       <div className="py-5 px-2">Cart is empty</div>
     </div>
   );

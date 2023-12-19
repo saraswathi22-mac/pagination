2728479d6641495f31bf5cab2329dba2dde572f6
exit <saraswathi.mac@gmail.com>
2023-12-19 15:55:29 +0530

    Make use of StoreProvider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import App from "@/components/App";
+import { StoreProvider } from "@/redux/StoreProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <App>{children}</App>
+        <StoreProvider>
+          <App>{children}</App>
+        </StoreProvider>
       </body>
     </html>
   );

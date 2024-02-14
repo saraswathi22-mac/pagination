34876c81d3d3c4b1101e70164376058b860ed4cf
exit <saraswathi.mac@gmail.com>
2024-02-14 19:53:02 +0530

    Updated Pagination

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import CartSliceReducer from "./slices/CartSlice";
-
-export const store = configureStore({
-  reducer: {
-    cart: CartSliceReducer,
-  },
-  devTools: process.env.NODE_ENV !== "production",
-});
